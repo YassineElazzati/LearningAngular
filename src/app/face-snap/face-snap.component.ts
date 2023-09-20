93a23943297773c419c19c52128174a2d4346b8c
YassineElazzati <yassine.elazzati@outlook.fr>
2023-09-20 17:01:11 +0200

    feat:Button

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit {
+  title! : string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+  imageUrl!:string;
 
+  ngOnInit(): void {
+      this.title = 'HOOOOO UN GATEEEEE';
+      this.description = 'Sch';
+      this.createdDate = new Date();
+      this.snaps = 6;
+      this.imageUrl = 'https://nypost.com/wp-content/uploads/sites/2/2023/03/NYPICHPDPICT000008985559.jpg'
+  }
+
+  onAddSnap() {
+    this.snaps++;
+  }
 }
